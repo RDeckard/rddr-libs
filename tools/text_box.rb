c2355bf6bd65c3e6980b6c696dfb5ecd52b2c595
Gabriel <rdeckard.gt@gmail.com>
2021-10-28 11:15:44 +0200

    UP TextBox: placeable

--- tools/text_box.rb
+++ tools/text_box.rb
@@ -1,15 +1,18 @@
 class RDDR::TextBox < RDDR::GTKObject
   attr_reader :frame, :text_lines
 
-  def initialize(text_lines, text_size: 0, frame_alignment: :center, frame_alignment_v: :center, text_alignment: :left, text_offset: 5, frame_offset: 5, max_width: grid.w)
+  def initialize(text_lines, text_size: 0, container_rect: grid.rect, frame_alignment: :center, frame_alignment_v: :center, text_alignment: :left, text_offset: 5, frame_offset: 5, max_width: nil, frame_x: nil, frame_y: nil)
     @text_lines        = (text_lines.is_a?(String) ? text_lines.split("\n") : text_lines).flatten
     @text_size         = text_size
+    @container_rect    = container_rect
     @frame_alignment   = frame_alignment
     @frame_alignment_v = frame_alignment_v
     @text_alignment    = text_alignment
     @text_offset       = text_offset
     @frame_offset      = frame_offset
-    @max_width         = max_width
+    @max_width         = max_width || container_rect.w
+    @frame_x           = frame_x
+    @frame_y           = frame_y
 
     primitives
   end
@@ -19,20 +22,20 @@ class RDDR::TextBox < RDDR::GTKObject
 
     @primitives = []
 
-    frame.x =
+    frame.x ||=
       case @frame_alignment
       when Numeric then @frame_alignment
-      when :center then geometry.center_inside_rect_x(frame, grid).x
-      when :left   then grid.left.shift_right(@frame_offset)
-      when :right  then grid.right.shift_left(@frame_offset + frame.w)
+      when :center then geometry.center_inside_rect_x(frame, @container_rect).x
+      when :left   then @container_rect.left.shift_right(@frame_offset)
+      when :right  then @container_rect.right.shift_left(@frame_offset + frame.w)
       end
 
-    frame.y =
+    frame.y ||=
       case @frame_alignment_v
       when Numeric then @frame_alignment_v
-      when :center then geometry.center_inside_rect_y(frame, grid).y
-      when :top    then grid.top.shift_down(@frame_offset + frame.h)
-      when :bottom then grid.bottom.shift_up(@frame_offset)
+      when :center then geometry.center_inside_rect_y(frame, @container_rect).y
+      when :top    then @container_rect.top.shift_down(@frame_offset + frame.h)
+      when :bottom then @container_rect.bottom.shift_up(@frame_offset)
       end
 
     @primitives << RDDR::Frame.new(frame, background_color: :black).primitives
@@ -60,7 +63,7 @@ class RDDR::TextBox < RDDR::GTKObject
 
   def frame
     @frame ||= begin
-      longest_text_line = @text_lines.max_by { |line| line.size }
+      longest_text_line = @text_lines.max_by { |line| line.size } || ""
       line_w, @line_h = gtk.calcstringbox(longest_text_line, @text_size)
       line_w += 2*@text_offset
       frame_w = line_w + 2*@frame_offset > @max_width ? @max_width - 2*@frame_offset : line_w
@@ -72,7 +75,7 @@ class RDDR::TextBox < RDDR::GTKObject
       @line_h += @text_offset
       frame_h = @text_offset + @text_lines.size*@line_h
 
-      { w: frame_w, h: frame_h, a: 128 }
+      { x: @frame_x, y: @frame_y, w: frame_w, h: frame_h, a: 128 }
     end
   end
 end
