afd7b17f7be645b1f31e0b4af1ef2de5a69d6140
Gabriel <rdeckard.gt@gmail.com>
2022-03-22 17:45:44 +0100

    UP [Spriteable] Handle the sprite precise shape, taking in account any rotation. Useful for e.g. precise click detection on a rotated sprite.

--- spriteable.rb
+++ spriteable.rb
@@ -5,13 +5,73 @@ module RDDR::Spriteable
     :sprite
   end
 
-  # Warning: this matches well only with square rotated sprites
-  def rect
-    if (90 - angle % 180).abs < 45 # closer to "right" or "left" orientation (angle of 90 or 270 degrees)
-      { x: x.shift_left(CE.card_model.rotation_offset), y: y.shift_up(CE.card_model.rotation_offset), w: h, h: w }
-    else # closer to "up" or "down" orientation (angle of 0 or 180 degrees)
+  # Warning: this matches well only with square rotated sprites (even with after_rotation == true)
+  def rect(after_rotation: false)
+    if after_rotation && (90 - angle % 180).abs < 45 # closer to the "left" or "right" orientations (angle of 90 or 270 degrees)
+      { x: x.shift_left(rotation_offset), y: y.shift_up(rotation_offset), w: h, h: w }
+    else # closer to the "up" or "down" orientations (angle of 0 or 180 degrees)
       { x: x, y: y, w: w, h: h }
     end
+  end
+
+  # Returns the 4 corners of the sprite as points (taking in account any rotation)
+  def shape_corners
+    if (angle % 90).round(3).zero?
+      # Square rotation cases optimization
+      rect = rect(after_rotation: true)
+
+      [
+        { x: rect.x,          y: rect.y },
+        { x: rect.x + rect.w, y: rect.y },
+        { x: rect.x + rect.w, y: rect.y + rect.h },
+        { x: rect.x,          y: rect.y + rect.h }
+      ]
+    else
+      rotation_center = rotation_center() # temporary memöization
+
+      [
+        { x: x,     y: y },
+        { x: x + w, y: y },
+        { x: x + w, y: y + h },
+        { x: x,     y: y + h }
+      ].map { geometry.rotate_point(_1, angle, rotation_center) }
+    end
+  end
+
+  # Returns 4 lines as a box containing the sprite (taking in account any rotation)
+  def shape_lines
+    shape_corners = shape_corners() # temporary memöization
+
+    [
+      shape_corners[0].merge(x2: shape_corners[1].x, y2: shape_corners[1].y),
+      shape_corners[1].merge(x2: shape_corners[2].x, y2: shape_corners[2].y),
+      shape_corners[2].merge(x2: shape_corners[3].x, y2: shape_corners[3].y),
+      shape_corners[3].merge(x2: shape_corners[0].x, y2: shape_corners[0].y)
+    ]
+  end
+
+  # Works with any rotation (unlike GTK #inside_rect? methods)
+  def contains_point?(point)
+    if (angle % 90).round(3).zero?
+      # Square rotation cases optimization
+      point.inside_rect?(rect(after_rotation: true))
+    else
+      # Interesting point: this clause wouldn't work with square rotation cases! (ray_tests :left or :right is arbitrary with straight lines)
+      shape_lines.
+        map { |shape_line| geometry.ray_test(point, shape_line) }.
+        then do |ray_tests|
+          ray_tests[0] == ray_tests[2] && ray_tests[1] == ray_tests[3] || !!ray_tests.index(:on)
+        end
+    end
+  rescue => e
+    return if e.message[":inside_rect? failed"]
+
+    raise e
+  end
+
+  # non relative rotation center (angle_anchor_x and angle_anchor_y are relative)
+  def rotation_center
+    { x: x + w*angle_anchor.x, y: y + h*angle_anchor.y }
   end
 
   def merge!(**attributes)
