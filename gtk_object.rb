d046a5d11c68002c27ef3b75cae738ac36db3adc
Gabriel <rdeckard.gt@gmail.com>
2023-07-05 19:31:07 +0200

    `attr_gtk` doesn't seem to work properly

--- gtk_object.rb
+++ gtk_object.rb
@@ -1,10 +1,32 @@
 class RDDR::GTKObject
   include RDDR::Serializable
 
-  attr_gtk
+  def gtk
+    $gtk
+  end
 
   def args
     $gtk.args
+  end
+
+  def state
+    args.state
+  end
+
+  def grid
+    args.grid
+  end
+
+  def geometry
+    args.geometry
+  end
+
+  def inputs
+    args.inputs
+  end
+
+  def outputs
+    args.outputs
   end
 
   def debug_puts(...)
