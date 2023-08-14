a2eff7864c5872fba1495d47ecb92f3422d89d97
Gabriel <rdeckard.gt@gmail.com>
2023-08-14 15:10:57 +0200

    Add RDDR.animated_colors

--- rddr.rb
+++ rddr.rb
@@ -21,4 +21,17 @@ module RDDR
     args = [:classic, *args] if args.one?
     RDDR::Colors::SETS.dig(*args).dup
   end
+
+  def animated_colors(first_color, second_color, interval: 0.2)
+    interval_in_ticks = interval * 60
+
+    first_color = [:classic, first_color] unless first_color.is_a?(Array)
+    second_color = [:classic, second_color] unless second_color.is_a?(Array)
+
+    if ($gtk.args.state.tick_count / interval_in_ticks).floor.mod(2).zero?
+      RDDR::Colors::SETS.dig(*first_color).dup
+    else
+      RDDR::Colors::SETS.dig(*second_color).dup
+    end
+  end
 end
